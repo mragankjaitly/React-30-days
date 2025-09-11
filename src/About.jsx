2b3d10f3c9d5b4176b6c5946137fd9299f1aac52
Mragank Jaitly <84132608+mragankjaitly@users.noreply.github.com>
2025-09-12 01:23:03 +0530

    Day 8: Added React Router with Home, About, Contact, and Users route

---
+++ src/About.jsx
@@ -0,0 +1,5 @@
+function About(){
+    return <div>Welcome to About Page</div>
+}
+
+export default About;
--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-import Greeting from './Greeting.jsx'
+import Greeting from "./Greeting.jsx";
 
-import Name from './ProfileCard.jsx'
+import Name from "./ProfileCard.jsx";
 
-import ProductCard from './ProductCard.jsx'
+import ProductCard from "./ProductCard.jsx";
 
-import Counter from './Counter.jsx'
+import Counter from "./Counter.jsx";
 
-import ToggleMessage from './ToggleMessage.jsx'
+import ToggleMessage from "./ToggleMessage.jsx";
 
-import TheamToggle from './TheamToggle.jsx'
+import TheamToggle from "./TheamToggle.jsx";
 
-import Logger from './Logger.jsx'
+import Logger from "./Logger.jsx";
 
-import Clock from './Clock.jsx'
+import Clock from "./Clock.jsx";
 
-import TitleClock from "./TitleClock"
+import TitleClock from "./TitleClock";
 
-import UserList from './UserList.jsx'
+import UserList from "./UserList.jsx";
 
+import Home from "./Home.jsx";
+
+import About from "./About.jsx";
+
+import Contact from "./Contact.jsx";
+
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
-
   return (
     <>
       {/* <div>
@@ -41,36 +47,53 @@ function App() {
       <Counter />
     </div> */}
 
-    {/* <div>
+      {/* <div>
       <h1>
         <ToggleMessage/>
       </h1>
     </div> */}
 
-    {/* <div>
+      {/* <div>
       <h1>Day 3 recap + day 4 start</h1>
       <TheamToggle/>
     </div> */}
 
-    {/* <div>
+      {/* <div>
       <h1>Day 4: useEffect Basics</h1>
       <Logger/>
     </div> */}
 
-    {/* <div>
+      {/* <div>
       <h1>Day 5: Clock</h1>
       <Clock/>
     </div> */}
 
-    <TitleClock />
+      <TitleClock />
 
-    <div>
+      {/* <div>
       <h1>Day 6: useEffect with API</h1>
       <UserList />
-    </div>
+    </div> */}
 
+      <div></div>
+      <h1>Day 8: React Router</h1>
+
+      {/* Navigation */}
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link> |{" "}
+        <Link to="/contact">Contact</Link> |{" "}<Link to = "/users">Users</Link>
+      </nav>
+
+      {/* Define Router */}
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/users" element={<UserList />}></Route>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
